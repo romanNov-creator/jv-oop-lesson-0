0958d6385305eea9e5e717ced47ed6933d24e496
Roman Novikov <novikdev12@gmail.com>
2025-08-04 14:49:05 +0200

    made findByEmail method static, used foreach instead of for statement, and simplified login method

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -13,11 +13,7 @@ public class AuthenticationService {
      * Return false in any other cases.
      */
     public boolean login(String email, String password) {
-        UserService userService = new UserService();
-        User user = userService.findByEmail(email);
-        if (user != null && user.getPassword().equals(password)) {
-            return true;
-        }
-        return false;
+        User user = UserService.findByEmail(email);
+        return user != null && user.getPassword().equals(password);
     }
 }
